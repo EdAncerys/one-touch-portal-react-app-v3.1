de9b075daccddce3d1b10ec6cb5b62c9e7d1d92a
Ed <lookatemail@gmail.com>
2021-06-07 09:13:39 +0100

    Push latest code to git

--- src/components/LiveConnections/ContractCard.jsx
+++ src/components/LiveConnections/ContractCard.jsx
@@ -20,65 +20,68 @@ export default function CustomerCard({ pageData, setFindContract }) {
               </tr>
             </thead>
             <tbody>
-              {pageData.map((customer, index) => (
-                <tr key={customer._id.toString()}>
-                  <td key={customer._id.toString() + 'a'}>{index + 1}</td>
-                  <td key={customer._id.toString() + 'b'}>
-                    <div key={index + 1}>
-                      {customer.oneTouchCustomer.companyName}
-                    </div>
-                    <div key={index + 2} style={styles.bottomRow}>
-                      {customer.oneTouchCustomer.contactName}
-                    </div>
-                  </td>
-                  <td key={customer._id.toString() + 'c'}>
-                    <div key={index + 1}>
-                      {customer.oneTouchCustomer[0] && (
-                        <div>
-                          {
-                            customer.oneTouchCustomer[0].oneTouchCustomer
-                              .thoroughfare_number
-                          }{' '}
-                          {
-                            customer.oneTouchCustomer[0].oneTouchCustomer
-                              .premises_name
-                          }{' '}
-                          {
-                            customer.oneTouchCustomer[0].oneTouchCustomer
-                              .sub_premises
-                          }{' '}
-                          {
-                            customer.oneTouchCustomer[0].oneTouchCustomer
-                              .thoroughfare_name
-                          }{' '}
-                          {customer.oneTouchCustomer[0].oneTouchCustomer.county}
-                        </div>
-                      )}
-                    </div>
-                    <div key={index + 2} style={styles.bottomRow}>
-                      {customer.oneTouchCustomer.postcode}
-                    </div>
-                  </td>
-                  <td key={customer._id.toString() + 'd'}>
-                    <div key={index + 1}>
-                      {customer.oneTouchCustomer.companyName}
-                    </div>
-                    <div key={index + 2} style={styles.bottomRow}>
-                      {customer.oneTouchCustomer.contactName}
-                    </div>
-                  </td>
-                  <td key={customer._id.toString() + 'e'} style={styles.btn}>
-                    <Button
-                      onClick={() => setFindContract(customer._id)}
-                      id={customer._id}
-                      size="sm"
-                      className="shadow-none"
-                    >
-                      Contract Info
-                    </Button>
-                  </td>
-                </tr>
-              ))}
+              {pageData.map((customer, index) => {
+                let customerData = customer.oneTouchCustomer[0];
+                if (customerData)
+                  customerData = customer.oneTouchCustomer[0].oneTouchCustomer;
+                console.log(customerData);
+
+                return (
+                  // {findContract && (
+                  //   <ContractInfoCard
+                  //     pageData={pageData}
+                  //     findContract={findContract}
+                  //     setFindContract={setFindContract}
+                  //   />
+                  // )}
+
+                  <tr key={customer._id.toString()}>
+                    <td key={customer._id.toString() + 'a'}>{index + 1}</td>
+                    <td key={customer._id.toString() + 'b'}>
+                      <div key={index + 1}>
+                        {customer.oneTouchCustomer.companyName}
+                      </div>
+                      <div key={index + 2} style={styles.bottomRow}>
+                        {customer.oneTouchCustomer.contactName}
+                      </div>
+                    </td>
+                    <td key={customer._id.toString() + 'c'}>
+                      <div key={index + 1}>
+                        {customerData && (
+                          <div>
+                            {customerData.thoroughfare_number}{' '}
+                            {customerData.premises_name}{' '}
+                            {customerData.sub_premises}{' '}
+                            {customerData.thoroughfare_name}{' '}
+                            {customerData.county}
+                          </div>
+                        )}
+                      </div>
+                      <div key={index + 2} style={styles.bottomRow}>
+                        {customerData.postcode}
+                      </div>
+                    </td>
+                    <td key={customer._id.toString() + 'd'}>
+                      <div key={index + 1}>
+                        {customer.oneTouchCustomer.companyName}
+                      </div>
+                      <div key={index + 2} style={styles.bottomRow}>
+                        {customer.oneTouchCustomer.contactName}
+                      </div>
+                    </td>
+                    <td key={customer._id.toString() + 'e'} style={styles.btn}>
+                      <Button
+                        onClick={() => setFindContract(customer._id)}
+                        id={customer._id}
+                        size="sm"
+                        className="shadow-none"
+                      >
+                        Contract Info
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </Table>
         </Card.Body>
